f0f33b7067cffd8fc261d42cfe72d829814f3416
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-05-09 17:20:22 +0530

    Add files via upload

---
+++ jsf2-pagination/src/main/java/com/jeejava/jsf/hibernate/domain/Cds.java
@@ -0,0 +1,54 @@
+package com.jeejava.jsf.hibernate.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cds")
+public class Cds implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	private String title;
+
+	private String interpret;
+
+	public Cds() {
+	}
+
+	public Cds(String title, String interpret) {
+		this.title = title;
+		this.interpret = interpret;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getInterpret() {
+		return this.interpret;
+	}
+
+	public void setInterpret(String interpret) {
+		this.interpret = interpret;
+	}
+
+}
