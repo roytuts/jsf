6becec8a5f9965b62dddadae1abfb03f15003d74
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-05-13 15:04:34 +0530

    Add files via upload

---
+++ jsf2-file-upload/src/main/java/com/jeejava/jsf/util/Utils.java
@@ -0,0 +1,21 @@
+package com.jeejava.jsf.util;
+
+import javax.servlet.http.Part;
+
+public class Utils {
+
+	private Utils() {
+	}
+
+	public static String getFileNameFromPart(Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		for (String content : partHeader.split(";")) {
+			if (content.trim().startsWith("filename")) {
+				String fileName = content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+				return fileName;
+			}
+		}
+		return null;
+	}
+
+}
