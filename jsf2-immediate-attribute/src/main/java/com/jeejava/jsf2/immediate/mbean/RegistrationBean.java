f9396330cf7d0a4b9ab5d5aa93b0d5cc3d2f20af
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-09-02 19:21:10 +0530

    Create RegistrationBean.java

---
+++ jsf2-immediate-attribute/src/main/java/com/jeejava/jsf2/immediate/mbean/RegistrationBean.java
@@ -0,0 +1,58 @@
+package com.jeejava.jsf2.immediate.mbean;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ViewScoped
+@ManagedBean
+public class RegistrationBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String gender;
+	private String dateOfBirth;
+	private String emailAddress;
+
+	public String register() {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("You have been successfully registered"));
+		return null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+}
