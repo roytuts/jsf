70e69d881622d9942528b21f72a3d4a60377eac1
Soumitra <email@email.com>
2021-05-18 13:56:28 +0530

    updated

--- jsf2-file-upload/src/main/java/com/roytuts/jsf/mbean/FileUploadMBean.java
+++ jsf2-file-upload/src/main/java/com/roytuts/jsf/mbean/FileUploadMBean.java
@@ -1,16 +1,13 @@
 package com.roytuts.jsf.mbean;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import javax.servlet.http.Part;
 
 import com.roytuts.jsf.util.Utils;
@@ -19,6 +16,7 @@ import com.roytuts.jsf.util.Utils;
 @ViewScoped
 public class FileUploadMBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	private Part file1;
 	private Part file2;
 	private String message;
@@ -48,64 +46,57 @@ public class FileUploadMBean implements Serializable {
 	}
 
 	public String uploadFile() throws IOException {
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-		FacesContext context = FacesContext.getCurrentInstance();
-		ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
-		String path = servletContext.getRealPath("");
 		boolean file1Success = false;
+
 		if (file1 != null && file1.getSize() > 0) {
 			String fileName = Utils.getFileNameFromPart(file1);
+
 			/**
 			 * destination where the file will be uploaded
 			 */
-			File outputFile = new File(path + File.separator + "WEB-INF" + File.separator + fileName);
-			inputStream = file1.getInputStream();
-			outputStream = new FileOutputStream(outputFile);
-			byte[] buffer = new byte[1024];
-			int bytesRead = 0;
-			while ((bytesRead = inputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0, bytesRead);
+			File savedFile = new File("C:/eclipse_2021_03_R/jsf2-file-upload", fileName);
+
+			// System.out.println("savedFile.toPath(): " + savedFile.toPath());
+
+			try (InputStream input = file1.getInputStream()) {
+				Files.copy(input, savedFile.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			if (outputStream != null) {
-				outputStream.close();
-			}
-			if (inputStream != null) {
-				inputStream.close();
-			}
+
 			file1Success = true;
 		}
+
 		boolean file2Success = false;
+
 		if (file2 != null && file2.getSize() > 0) {
 			String fileName = Utils.getFileNameFromPart(file2);
-			File outputFile = new File(path + File.separator + "WEB-INF" + File.separator + fileName);
-			inputStream = file2.getInputStream();
-			outputStream = new FileOutputStream(outputFile);
-			byte[] buffer = new byte[1024];
-			int bytesRead = 0;
-			while ((bytesRead = inputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0, bytesRead);
+			File savedFile = new File("C:/eclipse_2021_03_R/jsf2-file-upload", fileName);
+
+			System.out.println(savedFile.getAbsolutePath());
+
+			try (InputStream input = file2.getInputStream()) {
+				Files.copy(input, savedFile.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			if (outputStream != null) {
-				outputStream.close();
-			}
-			if (inputStream != null) {
-				inputStream.close();
-			}
+
 			file2Success = true;
 		}
+
 		if (file1Success || file2Success) {
-			System.out.println("File uploaded to : " + path);
+			// System.out.println("File uploaded to : " + path);
 			/**
 			 * set the success message when the file upload is successful
 			 */
-			setMessage("File successfully uploaded to " + path);
+			setMessage("File successfully uploaded");
 		} else {
 			/**
 			 * set the error message when error occurs during the file upload
 			 */
 			setMessage("Error, select atleast one file!");
 		}
+
 		/**
 		 * return to the same view
 		 */
